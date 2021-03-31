64df9269573517669643aad71b19f00a54aac9d0
sarahghossen <ghossensarah@gmail.com>
2021-03-31 13:01:53 -0400

    changes in productgrid - button added

--- client/src/components/ProductPage.js
+++ client/src/components/ProductPage.js
@@ -6,7 +6,22 @@ import Button from "./Button";
 
 const ProductPage = () => {
   const [item, setItem] = React.useState(null);
+  const [quantity, setQuantity] = React.useState(1);
+  const [quantityInputValue, setQuantityInputValue] = React.useState(1);
+
   let { id } = useParams();
+
+  const handlePlusClick = () => {
+    setQuantity(quantity + 1);
+    setQuantityInputValue(quantityInputValue + 1);
+  };
+
+  const handleMinusClick = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+      setQuantityInputValue(quantityInputValue - 1);
+    }
+  };
 
   React.useEffect(() => {
     fetch(`/item/${id}`)
@@ -20,21 +35,40 @@ const ProductPage = () => {
   return (
     <>
       {item ? (
-        <div>
-          <img src={item.imageSrc} />
-          <div>
-            <p>{item.name}</p>
-            <p>{item.price}</p>
+        <ProductDiv>
+          <ProductImg src={item.imageSrc} />
+          <ProductDiv2>
+            <Name>{item.name}</Name>
+            <Price>{item.price}</Price>
             <Button disabled={item.numInStock <= 0}>
               {item.numInStock <= 0 ? "Out of Stock" : "Add to Cart"}
             </Button>
-          </div>
-        </div>
+            <MainInput>
+              <Button onClick={handleMinusClick}>-</Button>
+              <Input
+                type="text"
+                value={quantityInputValue}
+                onChange={(ev) => {
+                  setQuantityInputValue(Number(ev.target.value));
+                }}
+              ></Input>
+              <Button onClick={handlePlusClick}>+</Button>
+            </MainInput>
+          </ProductDiv2>
+        </ProductDiv>
       ) : (
         <div>Loading...</div>
       )}
     </>
   );
 };
+
+const ProductDiv = styled.div``;
+const ProductDiv2 = styled.div``;
+const ProductImg = styled.img``;
+const Name = styled.p``;
+const Price = styled.p``;
+const MainInput = styled.div``;
+const Input = styled.div``;
 
 export default ProductPage;
